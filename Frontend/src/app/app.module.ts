847cd8d406ec22b8617038a0dc3f6202473d5c9e
Moh.Alkasem <49536603+malkasem@users.noreply.github.com>
2022-06-23 13:16:04 +0200

    Footer

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
